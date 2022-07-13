0de7f005372b83b902d30451851f3f52b932ddf2
JtheFox <altbammer@gmail.com>
2022-07-13 11:28:31 -0400

    refactor

---
+++ assets/info.js
@@ -0,0 +1,85 @@
+const links = [
+    {
+        "name": "Github",
+        "description": "My Github",
+        "url": "https://github.com/JtheFox/",
+        "active": true,
+        "fa_icon": "fa-brands fa-github"
+    },
+    {
+        "name": "LinkedIn",
+        "description": "My LinkedIn",
+        "url": "https://www.linkedin.com/in/jason-fox-878262197/",
+        "active": false,
+        "fa_icon": "fa-brands fa-linkedin"
+    },
+    {
+        "name": "Resume",
+        "description": "My CV",
+        "url": "#",
+        "active": false,
+        "fa_icon": "fa-solid fa-file-lines"
+    }
+]
+
+const linkTemplate = {
+    "name": "",
+    "description": "",
+    "url": "",
+    "active": true,
+    "fa_icon": ""
+}
+
+const projects = [
+    {
+        "date": "April 2022",
+        "title": "Chopping Block",
+        "image": "https://i.imgur.com/Y5MsQDC.png",
+        "live": "#",
+        "repo": "https://github.com/FidelHen/Chopping-Block",
+        "keywords": ["React Native", "Firebase"]
+    },
+    {
+        "date": "May 2022",
+        "title": "Password Generator",
+        "image": "https://i.imgur.com/F24PmtE.png",
+        "live": "https://jthefox.github.io/password-generator/",
+        "repo": "https://github.com/JtheFox/password-generator",
+        "keywords": ["HTML", "CSS", "JS"]
+    },
+    {
+        "date": "May 2022",
+        "title": "Weather Dashboard",
+        "image": "https://i.imgur.com/IZ72hZt.png",
+        "live": "https://jthefox.github.io/weather-dashboard/",
+        "repo": "https://github.com/JtheFox/weather-dashboard",
+        "keywords": ["Bootstrap", "jQuery"]
+    },
+    {
+        "date": "June 2022",
+        "title": "Somewhere.",
+        "image": "https://i.imgur.com/AnQy3h7.png",
+        "live": "https://jthefox.github.io/somewhere-project/",
+        "repo": "https://github.com/JtheFox/somewhere-project",
+        "keywords": ["HTML", "CSS", "jQuery"]
+    },
+    {
+        "date": "July 2022",
+        "title": "Blastoff",
+        "image": "https://raw.githubusercontent.com/JtheFox/blastoff/main/public/images/launch.png",
+        "live": "https://blastoff2.herokuapp.com/",
+        "repo": "https://github.com/JtheFox/blastoff",
+        "keywords": ["Express", "Handlebars", "Sequelize"]
+    },
+]
+
+const projectTemplate = {
+    "date": "",
+    "title": "",
+    "image": "",
+    "live": "",
+    "repo": "",
+    "keywords": []
+}
+
+export { links, projects };
--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-import { links as myLinks, projects as myProjects } from './modules/info.js'
+import { links as myLinks, projects as myProjects } from './assets/info.js'
 import { faLink, textWave, projectCard } from './modules/components.js'
 
 $(function () {
     // add personal links to header
-    $('.header-links').append($(myLinks.links.map(link => faLink(link)).join('')));
+    $('.header-links').append($(myLinks.map(link => faLink(link)).join('')));
     // add projects to showcase
-    myProjects.projects.forEach(project => {
+    myProjects.forEach(project => {
         $('.project-cards').append(projectCard(project));
     });
     // create cool & fun wavy text
