92291754084b6fd6e509778b2c91a15594fe657e
JtheFox <altbammer@gmail.com>
2022-07-13 11:58:12 -0400

    Reverse project display order

--- script.js
+++ script.js
@@ -5,11 +5,7 @@ $(function () {
     // add personal links to header
     $('.header-links').append($(myLinks.map(link => faLink(link)).join('')));
     // add projects to showcase
-    myProjects.forEach(project => {
-        $('.project-cards').append(projectCard(project));
-    });
+    myProjects.reverse().forEach(project => $('.project-cards').append(projectCard(project)));
     // create cool & fun wavy text
     $('.projects > h2').append(textWave('My Projects', 'fast'));
-    // position project showcase scrollbar in the middle on mobile devices
-    $('.card-list').scrollLeft($('.card').width());
 });
